900c0be4d1a226a861918e92e6a87c912e6d55e8
Jonas Svenstrup Hansen <jonas.svenstrup@gmail.com>
2018-02-03 20:26:09 +0100

    Sketched structure of uvb2rgb filter transformation function

--- utilities.py
+++ utilities.py
@@ -7,4 +7,8 @@ Utilities for the delsimi simulation code.
 """
 
 # TODO: create function to convert RGB to Johnson filters and back
-#https://www.sciencedirect.com/science/article/pii/S0273117715005694?via%3Dihub
+def uvb2rgb(uvb):
+	# Assume the following conversion applies to space photometry
+	#https://www.sciencedirect.com/science/article/pii/S0273117715005694?via%3Dihub
+	rgb = uvb
+	return rgb
