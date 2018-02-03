9fd5e65d038b4144e5c24c5eaaf51a78700ddb56
Jonas Svenstrup Hansen <jonas.svenstrup@gmail.com>
2018-02-03 14:49:40 +0100

    Added utilities file that will contain RGB-Johnson filter conversion

---
+++ utilities.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Utilities for the delsimi simulation code.
+
+@author: jonas
+"""
+
+# TODO: create function to convert RGB to Johnson filters and back
+#https://www.sciencedirect.com/science/article/pii/S0273117715005694?via%3Dihub
